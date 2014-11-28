4be49063147cea0383cbbaa9ea14aef011c6eed5
Kerubo <lucykerubo763@gmail.com>
2014-11-28 13:26:47 +0300

    first commit

--- FINAL/app/views/pages/home.blade.php
+++ FINAL/app/views/pages/home.blade.php
@@ -1,23 +1,29 @@
-@extends('layouts.master')
-@section('content')
-	<div id="content"><!-- start content -->
-<div id="slider">
+<!DOCTYPE html>
+<html>
+<body background="http://www.sonicavenger.com/uploaded_images/ClearBlueSky-741012.jpg"  border:5px solid orange>
+<div class="wrap" style="width:100%;height:100%">
+	<div class="header" style="width:90%;height:20%;background-color:#fff;">
+	<div style="float:left;width:20%;;">
+	<img src="assets/images/ebotics.png" width="50px" height="50px"></div>
+	<div style="float:left;width:30%;background-color:#fff;margin-left:10px;">
+	<h2 style="text-align:center;color:orange;">E-Botics Kenya</h2></div>
+		<nav style="float:left;width:40%;height:40px;background-color:#fff;padding-top:10px;margin-left:10px; text-decoration: none;">
 
-<img src="http://blog.wmiafrica.org/wp-content/uploads/2013/09/9662943066_4269a28a2b_z.jpg" width="1000px" height="800px">
+<a href="/">HOME</a> |
+<a href="/">ABOUT</a> |
+<a href="/">TEAM </a> |
+
+<a href="/">CONTACTS</a>|
+<a href="home/index">ADMIN LOGIN</a>
+</nav>
+	</div>
+<div class="body" style="width:90%;height:60%;background:#fff;margin:0 auto;">
+
+<h1>hey</h1>
+<p>
+<a href="/signup"> LOGIN</a>|
+<a href="/login"> LOGIN</a></p>
 </div>
-<div id="container">
-<div id="slide">
-<a href="signup"><h1 style="background:#fff;color:#000;">USER LOGIN</h1></a>
 </div>
-<div id="slid">
-<br><br><br>
-<h1>Content</h1>
-</div>
-<div id="sli">
-<br><br><br>
-<br><br><br>
-<h1>Share<br><br><br>Contest<br><br><br>Group</h1>
-</div>
-</div>
-</div><!-- end content -->
-@stop
+</body>
+</html> 
